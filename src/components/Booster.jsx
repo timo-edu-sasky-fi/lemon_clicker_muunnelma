3d854bfcf51c20db25efe6647971a0d0d285c47f
timo-edu-sasky-fi <timo.hannukainen@edu.sasky.fi>
2023-09-16 12:54:32 +0300

    lisää booster - komponentin

---
+++ src/components/Booster.jsx
@@ -0,0 +1,15 @@
+function Booster(props) {
+
+    // Poimitaan komponentille välitetty value-arvo
+    const value = props.value;
+  
+    return (
+      <div className="booster">
+        {value} lemon / click
+      </div>
+    );
+  
+  }
+  
+  export default Booster;
+  
