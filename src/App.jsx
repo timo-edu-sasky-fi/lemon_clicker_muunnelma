97a1362137e72ba223669279cce961825ae07840
timo-edu-sasky-fi <timo.hannukainen@edu.sasky.fi>
2023-09-16 12:52:27 +0300

    lisää balance - komponentin

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import './App.css'
 import Lemon from './components/Lemon';
+import Balance from './components/Balance';
+
 
 function App() {
 
   return (
     <>
       <div>
+        <Balance total="157" />
         <Lemon />
       </div>  
     </>
---
+++ src/components/Balance.jsx
@@ -0,0 +1,16 @@
+function Balance(props) {
+
+    // Poimitaan komponentille välitetty total-arvo
+    const total = props.total;
+  
+    return (
+      <div className="balance">
+        <div>lemons</div>
+        <div className="balance_total">{total}</div>
+      </div>
+    );
+  
+  }
+  
+  export default Balance;
+  
