174f94bf80237602e2e4f9f2b395cafbd23ef6d1
timo-edu-sasky-fi <timo.hannukainen@edu.sasky.fi>
2023-09-17 10:10:21 +0300

    lisää menu komponentin ja kääre elementin

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Balance from './components/Balance';
 import Booster from './components/Booster';
 import Header from './components/Header';
 import { useState } from 'react';
+import Menu from './components/Menu';
 
 function App() {
 
@@ -16,15 +17,19 @@ function App() {
     }
 
     
-  return (
-    <>
-      <div>
-        <Header>lemon clicker</Header>
-        <Balance total={clicks} />
-        <Lemon onClick={handleClick} />
-        <Booster value="3.2" />
+    return (
+      <div className="root">
+        <div className="root_content">
+          <div className="container clicker">
+            <Header>lemon clicker</Header>
+            <Balance total={clicks} />
+            <Lemon onClick={handleClick} />
+            <Booster value="3.2" />
+          </div>
+        </div>
+        <Menu items={2} />
       </div>  
-    </>
-  )}
+    )
+  }
 
 export default App
