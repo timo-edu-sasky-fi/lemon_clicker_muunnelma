b708d20989838391ce345eb6a4170f37bc150d9e
timo-edu-sasky-fi <timo.hannukainen@edu.sasky.fi>
2023-09-24 13:22:53 +0300

    lisää tilastoarvojen tulostuksen

--- src/App.jsx
+++ src/App.jsx
@@ -11,9 +11,19 @@ function App() {
     // Luodaan tilamuuttuja, johon tallennetaan tuotelista.
     const [storeitems,setStoreitems] = useState(items);
 
-    // Luodaan tilamuuttuja, johon tallennetaan pelin laskennalliset tiedot.
-    const [stats, setStats] = useState({clicks: 0, balance: 0, increase: 1, itemstobuy: 0});
+    // Esitellään pelin laskennalliset alkuarvot.
+    const initialstats = {
+      clicks: 0,
+      balance: 0,
+      increase: 1,
+      itemstobuy: 0,
+      upgrades: 0,
+      collected: 0
+    }
 
+    // Luodaan tilamuuttuja, johon tallennetaan pelin laskennalliset tiedot.
+    const [stats, setStats] = useState(initialstats);
+    
     // Laskee niiden tuotteiden lukumäärän, joiden ostamiseen on varaa.
     const countBuyableItems = (items, balance) => {
       let total = 0;
@@ -32,6 +42,8 @@ function App() {
       newstats.balance = round(newstats.balance + newstats.increase,1);
       // Lasketaan ostettavissa olevien tuotteiden lukumäärä.
       newstats.itemstobuy = countBuyableItems(storeitems,newstats.balance);
+      // Kasvatetaan sitruunoiden keräysmäärää.
+      newstats.collected = round(newstats.collected + newstats.increase,1);
       // Tallennetaan päivitetty stats-muuttuja.
       setStats(newstats); 
     }
