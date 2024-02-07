ae9a31261cf1a82edb61434b50c7dc1c1297b172
timo-edu-sasky-fi <timo.hannukainen@edu.sasky.fi>
2024-02-07 10:31:42 +0200

    milestones added

--- src/components/Balance.jsx
+++ src/components/Balance.jsx
@@ -1,18 +1,47 @@
 import shortenNumber from '../utils/shortenNumber';
 
 function Balance(props) {
+  // Poimitaan komponentille välitetty total-arvo
+  const total = shortenNumber(props.total);
 
-    // Poimitaan komponentille välitetty total-arvo
-    const total = shortenNumber(props.total);
-  
-    return (
+  var level_up_text = 'Noviisi';
+
+  if (total > 5) {
+    level_up_text = 'Hyvä';
+  }
+  if (total > 10) {
+    level_up_text = 'Kehittyvä';
+  }
+  if (total > 15) {
+    level_up_text = 'Parempi jo';
+  }
+  if (total > 20) {
+    level_up_text = 'Aika hyvä';
+  }
+  if (total > 25) {
+    level_up_text = 'Alkaa olla hyvä';
+  }
+  if (total > 30) {
+    level_up_text = 'Melkein mestari';
+  }
+  if (total > 35) {
+    level_up_text = 'Nyt mestari!';
+  }
+  if (total > 40) {
+    level_up_text = 'Supermestari!';
+  }
+
+  return (
+    <div className='balance_container'>
+      <div className='level_up_text'>
+        {level_up_text}
+      </div>
       <div className="balance">
         <div>lemons</div>
         <div className="balance_total">{total}</div>
       </div>
-    );
-  
-  }
-  
-  export default Balance;
-  
+    </div>
+  );
+}
+
+export default Balance;
